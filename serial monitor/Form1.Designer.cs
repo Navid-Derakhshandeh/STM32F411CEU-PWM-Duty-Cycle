36295cb7d98beafd93b92fc58f945a971539d378
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2025-08-14 15:27:27 +0330

    STM32F411CEU

---
+++ serial monitor/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace serialm_onitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.graphPanel = new System.Windows.Forms.Panel();
+            this.labelDuty = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(450, 72);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorts.TabIndex = 0;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(478, 130);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // graphPanel
+            // 
+            this.graphPanel.Location = new System.Drawing.Point(12, 12);
+            this.graphPanel.Name = "graphPanel";
+            this.graphPanel.Size = new System.Drawing.Size(432, 397);
+            this.graphPanel.TabIndex = 2;
+            this.graphPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.graphPanel_Paint);
+            // 
+            // labelDuty
+            // 
+            this.labelDuty.AutoSize = true;
+            this.labelDuty.Location = new System.Drawing.Point(478, 184);
+            this.labelDuty.Name = "labelDuty";
+            this.labelDuty.Size = new System.Drawing.Size(35, 13);
+            this.labelDuty.TabIndex = 3;
+            this.labelDuty.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelDuty);
+            this.Controls.Add(this.graphPanel);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Panel graphPanel;
+        private System.Windows.Forms.Label labelDuty;
+    }
+}
+
---
+++ serial monitor/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace serialm_onitor
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        List<float> dutyData = new List<float>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxPorts.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (comboBoxPorts.SelectedItem == null) return;
+
+            serialPort = new SerialPort(comboBoxPorts.SelectedItem.ToString(), 9600);
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.Open();
+        }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string line = serialPort.ReadLine();
+
+            if (line.Contains("Duty Cycle:"))
+            {
+                string valueStr = line.Split(':')[1].Replace("%", "").Trim();
+                if (float.TryParse(valueStr, out float duty))
+                {
+                    dutyData.Add(duty);
+                    if (dutyData.Count > 100) dutyData.RemoveAt(0);
+
+                    Invoke(new Action(() =>
+                    {
+                        labelDuty.Text = $"Duty Cycle: {duty}%";
+                        graphPanel.Invalidate();
+                    }));
+                }
+            }
+        }
+
+        private void graphPanel_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.Black);
+
+            if (dutyData.Count < 2) return;
+
+            Pen pen = new Pen(Color.Lime, 2);
+            int w = graphPanel.Width;
+            int h = graphPanel.Height;
+
+            for (int i = 1; i < dutyData.Count; i++)
+            {
+                float x1 = (i - 1) * w / (float)dutyData.Count;
+                float y1 = h - (dutyData[i - 1] * h / 100f);
+                float x2 = i * w / (float)dutyData.Count;
+                float y2 = h - (dutyData[i] * h / 100f);
+                g.DrawLine(pen, x1, y1, x2, y2);
+            }
+        }
+    }
+}
